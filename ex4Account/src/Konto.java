bf68cce9688a535379816409ce8679f51335d5c9
Adrian <adrian.madejski@vp.pl>
2022-11-15 14:02:20 +0100

    Update Konto.java

--- ex4Account/src/Konto.java
+++ ex4Account/src/Konto.java
@@ -1,4 +1,18 @@
 public abstract class Konto {
+    /*
+    Zdefiniuj klasę abstrakcyjną Konto reprezentującą rachunek bankowy.
+Klasa posiada pola:
+• imie_nazwisko // imię i nazwisko właściciela rachunku
+• saldo // stan rachunku
+
+Klasa oprócz konstruktora z parametrami oraz getterów i setterów posiada metody:
+• wyswietlInfo – wyświetla w kolejnych liniach wszystkie informacje o rachunku
+• zwrocStanKonta – zwracająca stan konta
+• wpłata – zwiększa stan konta o przekazaną kwotę
+• wyplata – jeżeli to możliwe, zmniejsza saldo o kwotę zadaną parametrem, jeśli nie zwraca
+false
+
+     */
 
     protected String imie_nazwisko;
     protected double saldo;
@@ -24,17 +38,21 @@ public abstract class Konto {
         this.saldo = saldo;
     }
 
-    public double zwrocSaldoKonta(){
-        return this.saldo;
+    public String wyswietlInfo() {
+        return "Imię i nazwisko właściciela: " + imie_nazwisko + "\n + saldo: " + saldo;
     }
 
-    public void wplata(double wplata){
-        saldo+=wplata;
+    public double zwrocStanKonta() {
+        return saldo;
     }
 
-    public boolean wyplata(double wyplata){
-        if(saldo-wyplata>=0){
-            saldo-=wyplata;
+    public void wplata(double wplata) {
+        saldo += wplata;
+    }
+
+    public boolean wyplata(double wyplata) {
+        if(saldo-wyplata>=0) {
+            saldo -= wyplata;
             return true;
         }
         return false;
@@ -48,14 +66,18 @@ public abstract class Konto {
                 '}';
     }
 
-    public boolean transfer(double kwota, Konto konto){
-        if(saldo-kwota>=0){
+    /*W klasie Konto dodaj metodę logiczną transfer, która jako parametry dostaje kwotę
+    i konto bankowe i dokonuje jeśli to możliwe transferu kwoty z konta na rzecz którego
+    wykonywana jest metoda na konto przekazane parametrem.*/
+
+    public boolean transfer(double kwota, Konto konto) {
+        if(saldo-kwota >=0) {
             konto.wplata(kwota);
             this.wyplata(kwota);
             return true;
         }
         return false;
     }
-
-
 }
+//ta klasa jest abstrakcyjna nie mozna stworzyc instancji tej klasy
+//uzytkownik moze miec konto bedace  jedna z 3 podklas - ma 1 z 3 typow konta
