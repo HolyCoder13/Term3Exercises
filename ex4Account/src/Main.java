f9db5a8098828ddc46a2212c4eb8bf7d80aa1e8a
Adrian <adrian.madejski@vp.pl>
2022-11-15 14:04:27 +0100

    Update Main.java

--- ex4Account/src/Main.java
+++ ex4Account/src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
-    public static void main(String[] args){
-
-
-
+    public static void main(String[] args) {
+    Konto k1 = new KontoDebetowe("ala b",1234,2344);
+    System.out.println(k1.wyswietlInfo());
+    Konto k2 = new KontoOszczendnosciowe("a c",234,2);
+    k1.transfer(1000,k2);
+    System.out.println(k2.wyswietlInfo());
+    
     }
 }
