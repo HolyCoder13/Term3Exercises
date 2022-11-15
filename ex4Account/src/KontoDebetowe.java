c9f9c5ee7dbc8e19900040c57eb29e4a62257113
Adrian <adrian.madejski@vp.pl>
2022-11-15 14:01:13 +0100

    Update KontoDebetowe.java

--- ex4Account/src/KontoDebetowe.java
+++ ex4Account/src/KontoDebetowe.java
@@ -1,54 +1,33 @@
-public class KontoZOplatami extends Konto {
+public class KontoDebetowe extends Konto {
 
-    private static final int LICZBA_BEZPLATNYCH_TRANSAKCJI = 5;
-    private static final double OPLATA_ZA_TRANSAKCJE = 0.5;
-    private int liczbaTransakcji;
-
-    public KontoZOplatami(String imie_nazwisko, double saldo) {
+    private double debet;
+    public KontoDebetowe(String imie_nazwisko, double saldo,double debet) {
         super(imie_nazwisko, saldo);
-        this.liczbaTransakcji = 0;
+        this.debet = debet;
     }
-
-    public int getLiczbaTransakcji() {
-        return liczbaTransakcji;
-    }
-
-    public void setLiczbaTransakcji(int liczbaTransakcji) {
-        this.liczbaTransakcji = liczbaTransakcji;
-    }
-
-    @Override
-    public void wplata(double wplata) {
-        super.wplata(wplata);
-        liczbaTransakcji++;
+    public boolean czyDebet(){
+        return saldo<0;
     }
 
     @Override
     public boolean wyplata(double wyplata) {
-        liczbaTransakcji++;
-        return super.wyplata(wyplata);
+        if(saldo-debet>=wyplata){
+            saldo -= wyplata;
+            return true;
+        }
+        return false;
     }
 
-    public void naliczOPlateZaTransakcje(){
-        if (liczbaTransakcji-LICZBA_BEZPLATNYCH_TRANSAKCJI> 0)
-        saldo-=(liczbaTransakcji-LICZBA_BEZPLATNYCH_TRANSAKCJI)*OPLATA_ZA_TRANSAKCJE;
+    public String wyswietlInfo(){
+        return super.wyswietlInfo()+"mozliwy debet: "+ debet;
     }
 
-//    public boolean transfer(double kwota, Konto konto) {
-//        if(saldo+debet>=kwota) {
-//            konto.wplata(kwota);
-//            this.wyplata(kwota);
-//            return true;
-//        }
-//        return false;
-//    }
-    
-
-    //    wypłata, wpłata – nadpisujemy metodę z nadklasy dodając w każdej z nich inkrementację
-//    licznika transakcji
-//      naliczOPlateZaTransakcje – zmniejsza stan konta o kwotę opłat za płatne transakcje
-//            (powyzej limitu transakcji)
-//• wyswietlInfo – nadpisuje metodę z nadklasy, uwzględniając specyfikę konta
-//• oraz konstruktorem zerujacym liczbaTransakcji
-
+    public boolean transfer(double kwota, Konto konto) {
+        if(saldo+debet>=kwota) {
+            konto.wplata(kwota);
+            this.wyplata(kwota);
+            return true;
+        }
+        return false;
+    }
 }
